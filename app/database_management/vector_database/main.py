54b54dd316da7dfb104d2d1a9efd5cf00c5e044a
Gianlucca Colangelo <gianlucisnt@uba.ar>
2024-10-13 12:20:59 -0300

    changed to BAAI/bge-base-en-v1.5 for embedding of vector database faster than bert, and it remains testing

--- app/database_management/vector_database/main.py
+++ app/database_management/vector_database/main.py
@@ -1,20 +1,21 @@
 import os
 from app.database_management.vectorizer.bert import BertVectorizer
+from app.database_management.vectorizer.bert import HuggingFaceVectorizer
 from app.database_management.vector_database.vector_database import FaissVectorDatabase
 from app.database_management.vector_database.abstract_processing import AbstractProcessingService
 
 def main():
     # Define paths
     data_dir = 'data'
-    index_file = os.path.join(data_dir, 'faiss_index.bin')
-    metadata_file = os.path.join(data_dir, 'metadata.pkl')
+    index_file = os.path.join(data_dir, 'bge_vector_database_faiss_index.bin')
+    metadata_file = os.path.join(data_dir, 'bge_vector_database_metadata.pkl')
 
     # Ensure data directory exists
     os.makedirs(data_dir, exist_ok=True)
 
     # We choose a vectorizer, in this case we use BERT
-    vectorizer = BertVectorizer(model_name='bert-base-uncased')
-    vector_dimension = 768  # BERT base model output dimension
+    vectorizer = HuggingFaceVectorizer(model_name='BAAI/bge-base-en-v1.5')
+    vector_dimension = 768 # BERT base model output dimension
 
     # We choose a vector database admin, in this case we use Faiss
     vector_database = FaissVectorDatabase(dimension=vector_dimension, index_file=index_file, metadata_file=metadata_file)
